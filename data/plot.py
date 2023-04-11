53c88884ebfc6a523598b3bce637eff9e07ae344
StephenR19 <44614501+StephenR19@users.noreply.github.com>
2023-04-11 16:05:10 +0100

    fix currency label not changing

--- data/plot.py
+++ data/plot.py
@@ -1,6 +1,6 @@
 import math
 from config.config import config
-from data.shareddata import SELECTED_COIN
+from data.shareddata import shareddata
 
 class Plot:
     @staticmethod
@@ -51,7 +51,8 @@ class Plot:
 
     @staticmethod
     def caption(price, y, screen_width, font, draw, fill=None, currency_offset=-1, price_offset=60):
-        draw.text((currency_offset, y), config.currency[SELECTED_COIN][:3], font=font, fill=fill)
+        selected_coin = shareddata.SELECTED_COIN
+        draw.text((currency_offset, y), config.currency[selected_coin][:3], font=font, fill=fill)
         price_text = Plot.human_format(price, 8, 2)
         text_width, _ = draw.textsize(price_text, font)
         price_position = (((screen_width - text_width - price_offset) / 2) + price_offset, y)
